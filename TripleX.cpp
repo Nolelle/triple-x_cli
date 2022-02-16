77ecc9decc0b2b0618b369e6fd94ada954fae656
Edmund <eyu1@ualberta.ca>
2022-02-15 17:00:49 -0700

    working

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
-int main() {
-  std::cout << "You are a secret agent breaking into a secure server room. \n";
-  std::cout << "Enter the correct codes to continue... \n";
+void PrintIntroduction(int Difficulty) 
+{
+    std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty << " secure server room. \n";
+    std::cout << "Enter the correct codes to continue... \n\n";
+}
+
+bool PlayGame(int Difficulty)
+{
+  PrintIntroduction(Difficulty);
 
   const int CodeA = 4;
   const int CodeB = 3;
@@ -17,14 +23,36 @@ int main() {
   std::cout << "The codes multiple to give: " << CodeProduct << "\n";
 
   std::cout << "Please enter your three guesses separated with a space. \n";
-
-  std::cin >> GuessA;
-  std::cin >> GuessB;
-  std::cin >> GuessC;
+  std::cin >> GuessA >> GuessB >> GuessC;
 
   int GuessSum = GuessA + GuessB + GuessC;
   int GuessProduct = GuessA * GuessB * GuessC;
 
+  if (GuessSum == CodeSum && GuessProduct == CodeProduct) 
+  {
+    std::cout << "You win! \n";
+    return true;
+  }
+  else 
+  {
+    std::cout << "That is incorrect, you lose! \n";
+    return false;
+  }
+};
 
-  return 0;
-}
+int main() 
+{
+  int LevelDiffculty = 1;
+
+  while(true)
+  {
+    bool bLevelComplete = PlayGame(LevelDiffculty);
+    std::cin.clear(); // Clears any errors
+    std::cin.ignore(); //Discards the buffer
+    if (bLevelComplete)
+    {
+      ++LevelDiffculty;
+    } 
+  };   
+ return 0;
+};
